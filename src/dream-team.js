8d250ae51fb569bcdd987b00b158124d07d731c4
Acolapsys <timur_sh@bk.ru>
2020-09-10 16:25:26 +0500

    feat: make dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -5,5 +5,10 @@ const CustomError = require("../extensions/custom-error");
 
 
 module.exports = function createDreamTeam(members) {
-
+  if (!Array.isArray(members)) return false;
+  let cypher = members.reduce((names, name) => {
+    if (typeof name !== 'string') return names;  
+    return names + name.replace(/\s+/g, '').toUpperCase()[0];   
+  }, "")
+ return cypher.split("").sort().join("");
 };
