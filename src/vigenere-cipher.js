5cebd4ba9326bd6995a6a7730a4be8eee5a58945
Acolapsys <timur_sh@bk.ru>
2020-09-12 15:42:41 +0500

    feat: make vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction = true) {
+    this.direction = direction
+  }
+  encrypt(message, key) {
+    let result  =  '';
+    key = key.repeat(Math.ceil(message.length / key.length));
+    let i = 0;
+    let j = 0;
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+    while (i < message.length) {
+      if (message[i].match(/[A-Z]/)) result += String.fromCharCode(((message.charCodeAt(i) - 65 + key.charCodeAt(j) - 65) % 26) + 65)
+      else {
+        result += message[i];
+        i++;
+        continue;
+      }
+      i++
+      j++
+    }
+    return this.direction ? result : result.split('').reverse().join('')
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    let result  =  '';
+    key = key.repeat(Math.ceil(message.length / key.length));
+    let i = 0;
+    let j = 0;
+    message = message.toUpperCase();
+    key = key.toUpperCase()
+    while (i < message.length) {
+      if (message[i].match(/[A-Z]/)) result +=  String.fromCharCode(((message.charCodeAt(i) + 26 - key.charCodeAt(j)) % 26) + 65)
+      else {
+        result += message[i];
+        i++;
+        continue;
+      }
+      i++
+      j++
+    }
+    return this.direction ? result : result.split('').reverse().join('')
   }
 }
 
